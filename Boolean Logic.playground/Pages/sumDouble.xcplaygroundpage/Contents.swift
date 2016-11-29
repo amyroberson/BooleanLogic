02db82063c6f56ff1ae3b91f79eb701ac272fa19
Amy Roberson <amy@roberson.xyz>
2016-11-29 16:43:27 -0500

    sumDouble complete

--- Boolean Logic.playground/Pages/sumDouble.xcplaygroundpage/Contents.swift
+++ Boolean Logic.playground/Pages/sumDouble.xcplaygroundpage/Contents.swift
@@ -5,3 +5,16 @@ import Foundation
 var str = "Hello, playground"
 
 //: [Next](@next)
+
+func sumDouble(first: Int, second: Int) -> Int {
+    if first == second{
+        return((first + second) * 2)
+    } else {
+        return (first + second)
+    }
+}
+
+
+sumDouble(first: 1, second: 2) == 3
+sumDouble(first: 3, second: 2) == 5
+sumDouble(first: 2, second: 2) == 8
